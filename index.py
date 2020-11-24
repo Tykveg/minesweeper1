8d9eceddc99b91f477ab3a0167c9f8393ca8ff07
Alexander <37273400+Tykveg@users.noreply.github.com>
2020-11-24 13:24:51 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 import pygame
 import random
+import config as c
 
-k_mines = 99 #Количество мин/Amount of mines
-map_size = (35,25) #Размер карты/Map size
-sq_size = 30 #Размер одного квадрата в пикселях/Square size in px.
+k_mines = c.k_mines #Количество мин/Amount of mines
+map_size = c.map_size #Размер карты/Map size
+sq_size = c.sq_size #Размер одного квадрата в пикселях/Square size in px.
 
 
 
